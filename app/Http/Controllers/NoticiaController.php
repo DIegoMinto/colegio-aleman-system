9dc84c69499a61493cdb3c78152e0141df2f4511
Javier Minto Arze <diegominto2@gmail.com>
2026-06-15 11:44:58 -0400

    Debug noticias

--- app/Http/Controllers/NoticiaController.php
+++ app/Http/Controllers/NoticiaController.php
@@ -24,37 +24,39 @@ class NoticiaController extends Controller
         $request->validate([
             'titulo' => 'required|string|max:150',
             'contenido' => 'required|string',
-            'archivo' => 'nullable|file|mimes:jpg,jpeg,png,pdf|max:5120',
-        ], [
-            'titulo.required' => 'El título es obligatorio.',
-            'contenido.required' => 'El cuerpo de la noticia es obligatorio.',
-            'archivo.mimes' => 'Solo se permiten formatos JPG, PNG o PDF.',
-            'archivo.max' => 'El archivo no debe superar los 5MB.',
+            'archivo' => 'nullable',
         ]);
 
         $archivoUrl = null;
         $tipoArchivo = null;
 
-        // Si el profesor adjuntó un archivo
-        if ($request->hasFile('archivo')) {
-            $file = $request->file('archivo');
-            $extension = $file->getClientOriginalExtension();
+        try {
+            if ($request->hasFile('archivo')) {
+                $file = $request->file('archivo');
+                $extension = strtolower($file->getClientOriginalExtension());
 
-            $tipoArchivo = ($extension === 'pdf') ? 'pdf' : 'imagen';
+                $tipoArchivo = ($extension === 'pdf') ? 'pdf' : 'imagen';
 
-            $path = Storage::disk('s3')->put('noticias', $file, 'public');
+                $path = Storage::disk('s3')->put('noticias', $file, 'public');
 
-            $archivoUrl = Storage::disk('s3')->url($path);
+                $archivoUrl = Storage::disk('s3')->url($path);
+            }
+
+            Noticia::create([
+                'titulo' => $request->titulo,
+                'contenido' => $request->contenido,
+                'archivo_url' => $archivoUrl,
+                'tipo_archivo' => $tipoArchivo,
+            ]);
+
+            return redirect()->route('noticias.index')->with('success', '¡Noticia escolar publicada con éxito!');
+
+        } catch (\Exception $e) {
+            dd([
+                'Mensaje de Error' => $e->getMessage(),
+                'Archivo donde Falló' => $e->getFile(),
+                'Línea del Error' => $e->getLine()
+            ]);
         }
-
-        // Guardamos todo en PostgreSQL
-        Noticia::create([
-            'titulo' => $request->titulo,
-            'contenido' => $request->contenido,
-            'archivo_url' => $archivoUrl,
-            'tipo_archivo' => $tipoArchivo,
-        ]);
-
-        return redirect()->route('noticias.index')->with('success', '¡Noticia escolar publicada con éxito!');
     }
 }
